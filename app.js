1b062b044e324975ea6d86b0ef34721ef81e39b9
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-22 13:01:46 +0530

    12 - Added lodash utility js library to convert lower/uppercase and defined index and compose templates and defined methods on compose to add the post to the posts array.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require('ejs');
 const PORT = process.env.PORT || 3000;
+const _ = require('lodash');
 
 const app = express();
 
@@ -11,13 +12,14 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 
+let postsarray = [];
 
 const homeStartingContent = "Lacu s vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 app.get('/',function(req,res){
-    res.render('index',{hometext:homeStartingContent});
+    res.render('index',{hometext:homeStartingContent,posts:postsarray});
 });
 
 app.get('/about',function(req,res){
@@ -28,11 +30,19 @@ app.get('/contact',function(req,res){
     res.render('contact',{pagetitle:'Contact us',contacttext:contactContent})
 });
 
-app.get('/post:id',function(req,res){
-    const title = req.body.id;
-    const desc = req.body.desc;
-    res.render('post',{heading:title,content:desc})
+app.get('/compose',function(req,res){
+    res.render('compose');
 });
+
+app.post('/compose',function(req,res){
+    const post = {
+        title: _.upperFirst(req.body.posttitle),
+        desc: _.lowerCase(req.body.postdesc)
+    };
+    postsarray.push(post);
+    res.redirect('/');
+});
+
 
 
 
